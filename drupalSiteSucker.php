122d6124334953e1e46ff7830d16f531d5730502
Nick Henry <nickh@standingcloud.com>
2011-11-01 16:23:16 -0600

    Fixing license based on Dave's input

--- drupalSiteSucker.php
+++ drupalSiteSucker.php
@@ -11,15 +11,12 @@
  *
  * PHP version 5
  *
- * LICENSE: This source file is subject to version 3.01 of the PHP license
- * that is available through the world-wide-web at the following URI:
- * http://www.php.net/license/3_01.txt.  If you did not receive a copy of
- * the PHP License and are unable to obtain it through the web, please
- * send a note to license@php.net so we can mail you a copy immediately.
+ * LICENSE: You may not use, copy, or modify this software without the
+ * permission of Standing Cloud, Inc. Contact us at support@standingcloud.com
+ * to request permission.
  *
  * @author     Nicholas Henry <nichk@standingcloud.com>
  * @copyright  2011 Standing Cloud, Inc.
- * @license    http://www.php.net/license/3_01.txt  PHP License 3.01
  */
 
 /**
